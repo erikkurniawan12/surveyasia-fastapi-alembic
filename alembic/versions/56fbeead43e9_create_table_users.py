b78119bdd1600783eae0608180d5fcc7c3370f8f
erikkurniawan12 <erik.kurniawan.1201@gmail.com>
2022-11-07 23:10:41 +0700

    migrations table

---
+++ alembic/versions/56fbeead43e9_create_table_users.py
@@ -0,0 +1,24 @@
+"""create table users
+
+Revision ID: 56fbeead43e9
+Revises: 794b68f0d2f9
+Create Date: 2022-11-07 23:04:04.589909
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '56fbeead43e9'
+down_revision = '794b68f0d2f9'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
