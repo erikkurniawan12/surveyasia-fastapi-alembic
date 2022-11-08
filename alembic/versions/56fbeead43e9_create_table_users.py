3cb301b991ab43e3dd8a49769a57edb0e5cb24a9
erikkurniawan12 <erik.kurniawan.1201@gmail.com>
2022-11-08 19:43:45 +0700

    alembic revision

--- alembic/versions/56fbeead43e9_create_table_users.py
+++ alembic/versions/56fbeead43e9_create_table_users.py
@@ -11,14 +11,23 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '56fbeead43e9'
-down_revision = '794b68f0d2f9'
+down_revision = None
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    pass
+    op.create_table(
+        'dt_karyawan',
+        sa.Column('id_pegawai', sa.Integer, primary_key=True),
+        sa.Column('nama_pegawai', sa.String(255), nullable=False),
+        sa.Column('alamat_pegawai', sa.String(255)),
+        sa.Column('ttl_pegawai', sa.Date()),
+        sa.Column('telp_pegawai', sa.String(255)),
+        sa.Column('email_pegawai', sa.String(255), unique=True)
+    )
+    
 
 
 def downgrade():
-    pass
+    op.drop_table('dt_pegawai')
