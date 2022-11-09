340cadafc7c07f2bd3a5d7331b3c2cb320b693a1
erikkurniawan12 <erik.kurniawan.1201@gmail.com>
2022-11-09 17:38:32 +0700

    create tbl_users alembic

--- models/users.py
+++ models/users.py
@@ -11,15 +11,6 @@ from sqlalchemy import (
 from sqlalchemy.sql.sqltypes import Date
 
 metadata = MetaData()
-# Pegawai = Table(
-#     "dt_pegawai", metadata,
-#     Column("id_pegawai", Integer, primary_key=True, index=True),
-#     Column("nama_pegawai", String(255), nullable=False),
-#     Column("alamat_pegawai", String(255)),
-#     Column("ttl_pegawai", Date()),
-#     Column("telp_pegawai", String(255)),
-#     Column("email_pegawai", String(255)),
-# )
 
 
 tbl_users = Table(
